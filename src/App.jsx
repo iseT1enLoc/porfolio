490b472e415f8d48c044c9be7c3c9ac0af80d981
iseT1enLoc <locnvt.it@gmail.com>
2025-03-18 20:29:48 +0700

    fix bug deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-import Edit from './components/blog_ui';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Navbar from './components/navbar';
